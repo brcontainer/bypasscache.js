0d14d10cdee7860d7ba9c6da91f25b296e816ca6
Guilherme Nascimento <new_guil1503@hotmail.com>
2014-12-11 17:10:39 -0200

    Create reload-resources.js

---
+++ reload-resources.js
@@ -0,0 +1,59 @@
+function ReloadAllResources(msg) {
+    var s, l, j, i, r = [];
+    var rl = function(uri, c) {
+        if (window.XMLHttpRequest) {
+            xhr = new window.XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            xhr = new window.ActiveXObject("Microsoft.XMLHTTP");
+        } else {
+            c();
+        }
+
+        xhr.open("POST", r[i], true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                c();
+            }
+        };
+        xhr.send("");
+    };
+
+    s = document.getElementsByTagName("script");
+    l = document.getElementsByTagName("link");
+
+    i = 0;
+    for (j = s.length; i < j; ++i) {
+        if (s[i].src) {
+            r.push(s[i].src);
+        }
+    }
+
+    i = 0;
+    for (j = l.length; i < j; ++i) {
+        if (l[i].href) {
+            r.push(l[i].href);
+        }
+    }
+
+    i = -1;
+    j = r.length;
+
+    if (msg) {
+        document.body.innerHTML = msg;
+    }
+
+    if (j > 0) {
+        asyncLoop = function() {
+            ++i;
+            if (i < j) {
+                rl(r[i], asyncLoop);
+            } else {
+                window.location.reload(true);
+            }
+            
+        };
+        asyncLoop();
+    } else {
+        window.location.reload(true);
+    }
+}
